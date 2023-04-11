a37a9e5cfa8cf9e3382c95c894b1dacc4df778a5
mikhaellala <124018365+mikhaellala@users.noreply.github.com>
2023-04-11 11:23:05 +0700

    Add files via upload

---
+++ UAS YAYAYAYA-2/assets/data.js
@@ -0,0 +1,77 @@
+var writers = [
+  {
+    "nama": "Jon Emont",
+    "info": "Jon Emont is a Southeast Asia reporter based in Singapore. He writes about political and economic developments in Asia and has covered topics ranging from the spread of Covid-19 in Asia, to the Rohingya crisis, and the effects of automation in the developing world. Prior to joining the Journal in 2017, Jon worked as a freelancer in Indonesia where he wrote for the New York Times, Washington Post and Financial Times. He speaks Bahasa Indonesia.",
+    "foto": "jon.jpeg"
+  }
+];
+
+var categories = [
+ 
+    {
+      "id": 1,
+      "headline": "FIFA Removes Indonesia as Host of Under-20 World Cup After Calls for Israel Exclusion",
+      "author": "John Emont",
+      "tanggal": "March 29, 2023",
+      "foto": "fifa.jpg"
+      
+    },
+    {
+      "id": 2,
+      "headline": "Kings continue to reap benefits of drafting Keegan Murray, who now owns NBA's rookie 3-point record",
+      "author": "Brad Botkin",
+      "tanggal": "March 30, 2023",
+      "foto": "2.jpg"
+    },
+    {
+      "id": 3,
+      "headline": "Prime Volleyball League Season 2 Witnesses 55% Increase in TV Viewership",
+      "author": "Ritayan Basu",
+      "tanggal": "March 28, 2023",
+      "foto": "3.jpg"
+    },
+    {
+      "id": 4,
+      "headline": "Halle Bailey Spent 13 Hours a Day in Water Filming Parts of Little Mermaid, Praises Disney for Story Update: Ariel Does not Leave the Ocean for a Boy",
+      "author": "Zack Sharff",
+      "tanggal": "March 22, 2023",
+      "foto" : "4.jpg"
+      },
+      {
+        "id": 5,
+        "headline": "Selena Gomez grabs dinner with Zayn Malik sassistant amid dating rumors",
+        "author": "Bernie Zilio",
+        "tanggal": "March 29, 2023",
+        "foto": "6.jpg"
+      },
+
+      {
+        "id": 6,
+        "headline": "Former Indonesian Police Officer Convicted Over Deadly Soccer Disaster",
+        "author": "John Emont",
+        "tanggal": "March 16, 2023",
+        "foto": "soccer.jpg"
+      },
+      {
+        "id": 7,
+        "headline": "Indonesia Shows It Is Possible to Tame Rainforest Destruction",
+        "author": "John Emont",
+        "tanggal": "February 27, 2023",
+        "foto": "rainforest.jpg"
+      },
+      {
+        "id": 8,
+        "headline": "Chinese and Local Workers Clash at Indonesia Nickel Smelter, Leaving Two Dead",
+        "author": "John Emont",
+        "tanggal": "January 16, 2023",
+        "foto": "jokowi.jpg"
+        },
+        {
+          "id": 9,
+          "headline": "Indonesia Risks Confrontation With China Over Gas Project in South China Sea",
+          "author": "John Emont",
+          "tanggal": "January 11, 2023",
+            "foto": "china.jpg"
+        }
+      
+      ];
